c5e0e993e8319d3ef5d44dce545ba86bdcf722aa
will dunwoody <will.dunwoody@gmail.com>
2023-01-06 12:04:56 +0100

    added sections to site

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,9 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '../styles/Home.module.css'
+import { BsFillMoonStarsFill } from "react-icons/bs";
+import { AiFillTwitterCircle, AiFillLinkedin, AiFillGithub, } from "react-icons/ai";
+import profilephoto from "../public/profile-photo.jpg"
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -14,10 +17,46 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>
-        <section className="bg-blue-700 min-h-screen">
-          <h1>Hello This Is My New portfolio</h1>
+      <main className="bg-white px-10">
+        <section className="min-h-screen">
+          <nav className="py-10 mb-12 flex justify-between">
+            <h1 className="text-xl font-burtons">Will Dunwoody</h1>
+            <ul className="flex items-center">
+              <li>
+                <BsFillMoonStarsFill className="cursor-pointer text-2xl"/>
+              </li>
+              <li>
+                <a className="bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8" href="https://www.linkedin.com/in/willdunwoody/">Resume</a>
+              </li>
+            </ul>
+          </nav>
 
+          <div className="text-center p-10">
+            <h2 className="text-5xl py-2 text-teal-600 font-medium">Will Dunwoody</h2>
+            <h3 className="text-2xl py-2">Developer</h3>
+            <p className="text-md py-5 leading-8 text-gray-800">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugit sit repellat error, eaque consectetur debitis animi ducimus, quae maxime eligendi iste voluptas itaque, adipisci magni aliquam repellendus explicabo sint culpa.</p>
+          </div>
+
+          <div className="text-5xl flex justify-center gap-16 py-3 text-gray-600">
+            <a href="https://www.linkedin.com/in/willdunwoody/"><AiFillLinkedin /></a>
+            <a href="http://github.com/WillDunwoody"><AiFillGithub /></a>
+            <a href="https://twitter.com/WDunwood/"><AiFillTwitterCircle /></a>
+          </div>
+
+          <div className="relative mx-auto w-80 h-80 mt-20">
+            <Image src={profilephoto} alt={"profile"} className="rounded-full"/>
+          </div>
+        </section>
+        <section className="bg-black">
+          <div>
+            <h3 className="text-3xl py-1 text-white">Services I offer</h3>
+            <p className="text-md py-2 leading-8 text-gray-800">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus consectetur debitis consequuntur sequi necessitatibus libero, explicabo, nam eligendi accusamus quam voluptate quae nemo id temporibus eveniet aspernatur aut voluptas neque.
+            </p>
+          </div>
+          <div>
+            <h2>Insert Images here</h2>
+          </div>
         </section>
       </main>
     </>
